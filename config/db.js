6b24485641e31a7c47d2a6d74f363533fed6a469
Fredson Junior <fredsonjunior5209@gmail.com>
2024-12-03 18:45:24 -0500

    AJUSTE DO BANCO

--- config/db.js
+++ config/db.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 // Função para conectar ao MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://sensor_temp:w0M41OBF4Qr6A0KM@bdsensor.0jdkz.mongodb.net/');
+    const dbPassword = 'w0M41OBF4Qr6A0KM'; // Substitua pela senha correta
+    const uri = `mongodb+srv://sensor_temp:${dbPassword}@bdsensor.0jdkz.mongodb.net/?retryWrites=true&w=majority&appName=bdsensor`;
+
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log('Conectado ao MongoDB');
   } catch (err) {
     console.error('Erro ao conectar com MongoDB', err);
