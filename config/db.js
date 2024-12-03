d704adcee0a991be7adf2f36567f420791c44e69
Fredson Junior <fredsonjunior5209@gmail.com>
2024-12-03 18:48:14 -0500

    depre

--- config/db.js
+++ config/db.js
@@ -6,10 +6,7 @@ const connectDB = async () => {
     const dbPassword = 'w0M41OBF4Qr6A0KM'; // Substitua pela senha correta
     const uri = `mongodb+srv://sensor_temp:${dbPassword}@bdsensor.0jdkz.mongodb.net/?retryWrites=true&w=majority&appName=bdsensor`;
 
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri); // Não precisa mais de opções adicionais
 
     console.log('Conectado ao MongoDB');
   } catch (err) {
